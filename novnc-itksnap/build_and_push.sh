64544a2fdf20512bcf36e4b1357e76d5f66f8d5e
pangyuteng <pangyuteng@gmail.com>
2024-06-21 12:52:56 +0000

    wip

--- novnc-itksnap/build_and_push.sh
+++ novnc-itksnap/build_and_push.sh
@@ -1,2 +1,3 @@
-docker build -f Dockerfile.turbovnc -t pangyuteng/theonemule-tubovnc:latest .
-docker push pangyuteng/theonemule-tubovnc:latest
+docker build -f Dockerfile.turbovnc -t turbovnc .
+docker tag turbovnc pangyuteng/theonemule-turbovnc:latest
+docker push pangyuteng/theonemule-turbovnc:latest
